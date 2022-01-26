ee6aa699bebd6d953355775f879156a9b99d47f4
liskovich <ricardsliskovskis@gmail.com>
2022-01-26 13:21:06 +0200

    Add Folder Structure

---
+++ EnterpriseApp.Domain/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using EnterpriseApp.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseApp.Domain.Entities;
+
+public class Product : BaseEntity
+{
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    
+}
+
